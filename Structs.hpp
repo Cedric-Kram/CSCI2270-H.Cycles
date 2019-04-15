a5608866afe3fa27844900bbbafc39462d54ee38
Scholz4208 <49652683+Scholz4208@users.noreply.github.com>
2019-04-15 13:15:48 -0600

    Update Structs.hpp

--- Structs.hpp
+++ Structs.hpp
@@ -1,1 +1,30 @@
+#include <vector>
+using namespace std;
 
+struct Vertex{
+  int id;
+  bool visited = false;
+  Vertex(int num){id = num;}
+  vector<Vertex*> edges;
+}
+
+//Linked list stuff
+
+struct VertexLLNode{
+  Vertex *v;
+  Vertex *next = NULL;
+  Vertex *prev = NULL;
+  VertexLLNode(Vertex *t){v = t;}
+}
+
+class VertexLL{
+  private:
+    VertexLLNode head(NULL);
+    VertexLLNode tail(NULL):
+  public:
+    VertexLL();
+    ~VertexLL();
+    void append(Vertex *v);
+    void remove(int id);
+    VertexLLNode findVertexNode(int id);
+}
